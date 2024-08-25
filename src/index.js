10f41c0f7c4390da919eaf0c3f1362459eeb4dd2
Alexandra_V <alexandra.vukits82@gmail.com>
2024-08-25 13:19:47 +0200

    Added the AI API imigration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "This will be a gread salad!",
+    strings: response.data.answer,
     autoStart: true,
     delay: 5,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let apiKey = "40eebb051ec036c739feeto37ba5f2ee";
+  let instructionVariable = document.querySelector("#user-instructions");
+
+  let apiContext =
+    "You are a Recipe expert and love to create recipes. Your mission is to create detailed recipe in basic HTML. Make sure to follow the user instructions. ";
+  let apiPrompt = `User instructions: Generate a recipe about ${instructionVariable.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
+
+  console.log("Generating recipe");
+  console.log(`Prompt: ${apiPrompt}`);
+  console.log(`Context: ${apiContext}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let formVariable = document.querySelector("#generator-form");
